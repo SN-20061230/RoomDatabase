e8e598d26f137e73decdc2f935488c85d4d2d8a9
Handy2 <aqayumov96@gmail.com>
2023-11-27 15:38:26 +0500

    Hala Madrid

---
+++ app/src/main/java/com/example/roomdatabase/dBase/Contact.kt
@@ -0,0 +1,13 @@
+package com.example.roomdatabase.dBase
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "contacts")
+data class Contact(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val name: String,
+    val phone: String
+)
